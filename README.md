855d52b02f23d2dea469a8d94b158599dcb6fb52
nick <7zx.1zx@gmail.com>
2023-05-10 15:03:46 +0300

    setup .env

--- README.md
+++ README.md
@@ -12,3 +12,7 @@
 1. Install and activate virtual environment
 2. Install package manager [poetry](https://python-poetry.org/): `pip install poetry`
 3. Install dependencies: `poetry install`
+4. Create file named ".env" in the project root directory
+5. Fill ".env" with this lines:
+>SECRET_KEY='<your_secret_key>'  
+DEBUG=True
--- todolist/settings.py
+++ todolist/settings.py
@@ -12,21 +12,31 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+ENV_PATH = BASE_DIR.joinpath('.env')
+if ENV_PATH.is_file():
+    env.read_env(ENV_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0h#8eu)m_*$mruc%e#he9s&ofl=p=5-kd11$*6w+b_9rape6%0'
+# SECRET_KEY = 'django-insecure-0h#8eu)m_*$mruc%e#he9s&ofl=p=5-kd11$*6w+b_9rape6%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+SECRET_KEY = env.str('SECRET_KEY')
+DEBUG = env.bool('DEBUG')
 
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -69,7 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todolist.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -79,7 +88,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -99,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -110,7 +117,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/

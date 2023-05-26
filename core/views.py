9e7343f01722503d44f5c0ec7e451096e9bc1036
nick <7zx.1zx@gmail.com>
2023-05-26 15:13:21 +0300

    test ci/cd

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class StatusView(View):
+    def get(self, request):
+        return JsonResponse({"status": "ok"}, status=200)
--- todolist/urls.py
+++ todolist/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import StatusView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', StatusView.as_view()),
 ]

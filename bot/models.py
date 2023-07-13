aab69aba7c06330973a4cd1b5b3ea1aa52c063f0
nick <7zx.1zx@gmail.com>
2023-07-13 17:45:42 +0300

    edited models

--- bot/models.py
+++ bot/models.py
@@ -1,5 +1,5 @@
+import random
 import string
-
 
 from django.db import models
 
@@ -9,6 +9,19 @@ CODE_VOCABULARY = string.ascii_letters + string.digits
 
 
 class TgUser(models.Model):
-    tg_chat_id = models.BigIntegerField()
-    tg_user_id = models.BigIntegerField()
-    user_id = models.ForeignKey(User, models.PROTECT, null=True, blank=True, default=None, verbose_name='Пользователь')
+    chat_id = models.BigIntegerField()
+    user_id = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=512, verbose_name="tg username", null=True, blank=True, default=None)
+    user = models.ForeignKey(User, models.PROTECT, null=True, blank=True, default=None)
+    verification_code = models.CharField(max_length=32)
+
+    def set_verification_code(self):
+        code = "".join([random.choice(CODE_VOCABULARY) for _ in range(12)])
+        self.verification_code = code
+
+    def __str__(self):
+        return '{}'.format(self.user)
+
+    class Meta:
+        verbose_name = "tg User"
+        verbose_name_plural = "tg Users"
